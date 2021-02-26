a2936b5f8d74fcc2dcb934144dc4ce429f5214e8
lina384810 <lp.cardozo@uniandes.edu.co>
2021-02-26 15:47:24 -0500

    Modifying index.js

--- index.js
+++ index.js
@@ -29,26 +29,58 @@ const http = require("http");
   console.log(data);
 }); */
 
+const url1 =
+  "https://gist.githubusercontent.com/josejbocanegra/d3b26f97573a823a9d0df4ec68fef45f/raw/66440575649e007a9770bcd480badcbbc6a41ba7/proveedores.json";
+const url2 =
+  "https://gist.githubusercontent.com/josejbocanegra/986182ce2dd3e6246adcf960f9cda061/raw/f013c156f37c34117c0d4ba9779b15d427fb8dcd/clientes.json";
+
+const axios = require("axios");
+
+// Optionally the request above could also be done as
+axios
+  .get(url1)
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+
+axios
+  .get(url2)
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+
 const getFileContent = (callback) => {
-  fs.readFile("index.js", (err, data) => {
+  fs.readFile("index.html", (err, data) => {
     callback(data.toString());
   });
 };
 
-http
-  .createServer((req, res) => {
-    //res.end("Hola mundo");
-    getFileContent((data) => {
-      res.end(data);
-    });
-    //console.log("Hola mundo");
-    /*Este código permite hacer cosas como buscar
-    en una API una URL (ej: /api/albums/100 -> retorne
-    el album 100 del recurso albums dentro de api*/
-    // const obj = [{name: "ana"}, {name: "maria"}];
-    // console.log(req.url);
-    /*if ((req.url = "/api/albums")) {
-    } else {
-    }*/
-  })
-  .listen(8000);
+// http
+//   .createServer((req, res) => {
+//     //res.end("Hola mundo");
+//     getFileContent((data) => {
+//       res.end(data);
+//     });
+//     //console.log("Hola mundo");
+//     /*Este código permite hacer cosas como buscar
+//     en una API una URL (ej: /api/albums/100 -> retorne
+//     el album 100 del recurso albums dentro de api*/
+//     // const obj = [{name: "ana"}, {name: "maria"}];
+//     // console.log(req.url);
+//     /*if ((req.url = "/api/albums")) {
+//     } else {
+//     }*/
+//   })
+//   .listen(8000);
