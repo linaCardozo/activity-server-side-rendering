742a92b642cb4fdd9752b6e2929b895383074c8f
lina384810 <lp.cardozo@uniandes.edu.co>
2021-03-02 23:33:11 -0500

    Finished activity

--- index.js
+++ index.js
@@ -1,86 +1,88 @@
-// const fs = require("fs");
-
-// let fileContent = "";
-
-// const getFileContent = (callback) => {
-//     fs.readFile("index.js", (err, data) => {
-//         if (err) throw err;
-//         //fileContent = data.toString();
-//         callback(data.toString());
-//     });
-// };
-
-// //console.log("Data", fileContent);
-// //getFileContent((data) => console.log(data)); //Forma 1
-// getFileContent(console.log); // Otra forma de hacer lo de arriba
-
-// fs.writeFile("prueba.js", "contenido del archivo", (err)=> {
-//     if (err) throw err;
-//     console.log("Archivo creado correctamente");
-// });
-
-// Escuchar peticiones http
-
 const fs = require("fs");
 const http = require("http");
+const axios = require("axios");
 
-// Aquí el archivo está solo dentro del callback
-/* fs.readFile("index.js", (err, data) => {
-  console.log(data);
-}); */
-
+// URL from clients and suppliers
 const url1 =
   "https://gist.githubusercontent.com/josejbocanegra/d3b26f97573a823a9d0df4ec68fef45f/raw/66440575649e007a9770bcd480badcbbc6a41ba7/proveedores.json";
 const url2 =
   "https://gist.githubusercontent.com/josejbocanegra/986182ce2dd3e6246adcf960f9cda061/raw/f013c156f37c34117c0d4ba9779b15d427fb8dcd/clientes.json";
 
-const axios = require("axios");
+let table = "";
 
-// Optionally the request above could also be done as
-axios
-  .get(url1)
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
-
-axios
-  .get(url2)
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
-
-const getFileContent = (callback) => {
-  fs.readFile("index.html", (err, data) => {
+// Getting suppliers page (proveedores.html)
+const getSuppliersPage = (callback) => {
+  fs.readFile("proveedores.html", (err, data) => {
+    if (err) throw err;
     callback(data.toString());
   });
 };
 
-// http
-//   .createServer((req, res) => {
-//     //res.end("Hola mundo");
-//     getFileContent((data) => {
-//       res.end(data);
-//     });
-//     //console.log("Hola mundo");
-//     /*Este código permite hacer cosas como buscar
-//     en una API una URL (ej: /api/albums/100 -> retorne
-//     el album 100 del recurso albums dentro de api*/
-//     // const obj = [{name: "ana"}, {name: "maria"}];
-//     // console.log(req.url);
-//     /*if ((req.url = "/api/albums")) {
-//     } else {
-//     }*/
-//   })
-//   .listen(8000);
+// Getting clientes page (clientes.html)
+const getClientsPage = (callback) => {
+  fs.readFile("clientes.html", (err, data) => {
+    if (err) throw err;
+    callback(data.toString());
+  });
+};
+
+// Getting data of suppliers from URL
+const getSuppliers = (callback) => {
+  axios
+    .get(url1)
+    .then(function (response) {
+      callback(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+
+// Getting data of clients from URL
+const getClients = (callback) => {
+  axios
+    .get(url2)
+    .then(function (response) {
+      callback(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+
+// Creating server for building the HTML of clients' and providers' tables
+// The server listens on port 8081
+http
+  .createServer((req, res) => {
+    table =
+      '<table class="table table-striped"><thead><tr><th>ID</th><th>Nombre Compañía</th><th>Contacto</th></tr></thead><tbody>';
+
+    if (req.url === "/api/clientes") {
+      getClientsPage((data) => {
+        getClients((cli) => {
+          for (let i = 0; i < cli.data.length; i++) {
+            table += "<tr><td>" + cli.data[i].idCliente + "</td>";
+            table += "<td>" + cli.data[i].NombreCompania + "</td>";
+            table += "<td>" + cli.data[i].NombreContacto + "</td>";
+            table += "</tr>";
+          }
+          table += "</tbody></table>";
+          res.end(data + table);
+        });
+      });
+    } else if (req.url === "/api/proveedores") {
+      getSuppliersPage((data) => {
+        getSuppliers((sup) => {
+          for (let i = 0; i < sup.data.length; i++) {
+            table += "<tr><td>" + sup.data[i].idproveedor + "</td>";
+            table += "<td>" + sup.data[i].nombrecompania + "</td>";
+            table += "<td>" + sup.data[i].nombrecontacto + "</td>";
+            table += "</tr>";
+          }
+          table += "</tbody></table>";
+          res.end(data + table);
+        });
+      });
+    }
+  })
+  .listen(8081);
